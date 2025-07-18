2ed51fc467adff724087b6214f242774e4661bc1
Abdullah Rapheo <abdullah.rapheo@g.bracu.ac.bd>
2025-07-18 18:01:52 +0600

    added clear terminal

--- PlaneLocation.py
+++ PlaneLocation.py
@@ -1,8 +1,13 @@
 import requests
 import keyboard
+import os
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 lat, lon = 23.7993984, 90.423296
-delta = 0.027   # approx 3km or 3000 meter
+delta = 0.1   # approx 11km or 11000 meter
 
 url = (
     f"https://opensky-network.org/api/states/all"
@@ -13,15 +18,23 @@ url = (
 )
 r = requests.get(url)
 data = r.json()
-
+count = 0
 while True:
     if keyboard.is_pressed("q"):
         print("q pressed, ending loop")
         break
+
+    if keyboard.is_pressed("e"):
+        clear_terminal()
+        print("terminal cleared")
     
     if not data.get("states"):
-        print("No airborne flights within ~3 km.")
+        print("No airborne flights within ~11 km.")
     else:
+        if count == 0:
+            clear_terminal()
+            count = count + 1
+
         print("Flights overhead:")
         print(data["states"])
         for s in data["states"]:
